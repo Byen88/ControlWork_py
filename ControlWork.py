5809fe1b4c88f00ad6b4674e0672e7c3bd59fda3
byen88 <byen@mail.ru>
2023-10-15 16:10:14 +0300

    final commit

--- ControlWork.py
+++ ControlWork.py
@@ -3,21 +3,19 @@
 # Ваша задача перевести его в one hot вид. 
 # Сможете ли вы это сделать без get_dummies?
 
-import pandas as pd 
-import numpy as np 
 import random
- 
-lst = ['robot'] * 10
-lst += ['human'] * 10
-random.shuffle(lst)
-data = pd.DataFrame({'whoAmI': lst})
-print(data)
- 
-print('')
 
-data['tmp'] = 1
-data.set_index([data.index, 'whoAmI'], inplace=True)
-data = data.unstack(level=-1, fill_value = 0).astype(int)
-data.columns = data.columns.droplevel()
-data.columns.name = None
-print(data)
+lst = ['robot'] * 10 + ['human'] * 10
+random.shuffle(lst)
+data = [{'whoAmI': item} for item in lst]
+
+for entry in data:
+    print(entry)
+
+encoded_data = []
+for entry in data:
+    encoded_entry = {f'whoAmI_{entry["whoAmI"]}': 1}
+    encoded_data.append(encoded_entry)
+
+for entry in encoded_data:
+    print(entry)
